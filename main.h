c94ac18129353ca0f7d1a957885af7586328adb3
P4ver <s.pales20@gmail.com>
2023-09-18 12:03:41 -0700

    sh

--- main.h
+++ main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-
-#define BUFFER_SIZE 1024
 
 int _printf(const char *format, ...);
 
