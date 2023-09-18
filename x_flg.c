b3d5b63e87d0f5613fb541da60a1f0573d4697b5
assiaboudlal98 <boudlala22@gmail.com>
2023-09-18 14:51:43 -0700

    a

--- x_flg.c
+++ x_flg.c
@@ -10,25 +10,25 @@ int x_flg(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int k, curr_i;
 	int flg = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char FLG_P[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLG_y[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (etg_i = *i + 1; format[etg_i] != '\0'; etg_i++)
 	{
-		for (j = 0; FLG_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLG_CH[j])
+		for (k = 0; FLG_CH[k] != '\0'; k++)
+			if (format[etg_i] == FLG_CH[j])
 			{
 				flg |= FLG_ARR[j];
 				break;
 			}
 
-		if (FLG_CH[j] == 0)
+		if (FLG_CH[k] == 0)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = etg_i - 1;
 
 	return (flg);
 }
