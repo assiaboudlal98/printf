93a07dd3e6204893a0d9fe6699b9e20cef941916
P4ver <s.pales20@gmail.com>
2023-09-19 12:30:48 -0700

    tsk tsk

--- main.h
+++ main.h
@@ -1,12 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+
 /* main.c */
 int _printf(const char *format, ...);
+
 /* _putchar.c */
 int _putchar(char c);
+
 /* printf_functions.c */
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
+
 #endif /* MAIN_H */
