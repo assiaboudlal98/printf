0b7bd3b2f036a1c85ce6f5c2ff6a633b3b2de19b
P4ver <s.pales20@gmail.com>
2023-09-19 14:49:26 -0700

    oo

--- _printf1.c
+++ _printf1.c
@@ -5,52 +5,51 @@
 int _printf(const char *format, ...)
 {
 	va_list args_arx;
-	int cnt_x;
+	int cnt_x = 0;
 
 	va_start(args_arx, format);
-	cnt_x = 0;
-
 	if (!format || !format[0])
 		return (-1);
 	while (*format)
 	{
-		if (*format == '%')
+	if (*format == '%')
+	{
+		format++;
+		if (*format == 'c')
 		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args_arx, int);
-				cnt_x += _putchar(c);
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args_arx, char*);
+		char c = va_arg(args_arx, int);
 
-				cnt_x += hnd_str(s);
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				cnt_x++;
-			}
-			else
-			{
+		cnt_x += _putchar(c);
+		}
+		else if (*format == 's')
+		{
+		char *s = va_arg(args_arx, char *);
+
+		cnt_x += hnd_str(s);
+		}
+		else if (*format == '%')
+		{
 			_putchar('%');
 			cnt_x++;
-			if (*format)
-			{
-			_putchar(*format);
-			cnt_x++;
-			}
-			}
-			format++;
 		}
 		else
 		{
-			_putchar(*format);
-			format++;
-			cnt_x++;
+		_putchar('%');
+		cnt_x++;
+		if (*format)
+		{
+		_putchar(*format);
+		cnt_x++;
 		}
+		}
+		format++;
+	}
+	else
+	{
+		_putchar(*format);
+		format++;
+		cnt_x++;
+	}
 	}
 	va_end(args_arx);
 	return (cnt_x);
