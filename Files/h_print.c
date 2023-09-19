365a9d5a15557d74a5adb8ca40f058d46b623c5e
P4ver <s.pales20@gmail.com>
2023-09-19 12:32:06 -0700

    tsk

---
+++ Files/h_print.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * h_print - looking for its type it prints the argument
+ * @fmt: to prints the arguments and format the string.
+ * @list: argument's list to be printed
+ * @ind: ind.
+ * @buffer: array's buffer for handling print.
+ * @flags: for Calculating active flags
+ * @width: get wdt.
+ * @precision: for Precision specification
+ * @size: telling the size
+ * Return: 1 or 2;
+ */
+int h_print(const char *jtn, int *ind, va_list list, char buffer[],
+	int flg, int width, int prcs, int sz)
+{
+	int i, unknown_eln = 0, prt_char = -1;
+	jtn_s jtn_types[] = {
+		{'c', pr_char}, {'s', print_string}, {'%', pr_prcnt},
+		{'i', print_int}, {'d', print_int}, {'b', pr_binary},
+		{'u', print_uns}, {'o', print_oct}, {'x', pr_hex},
+		{'X', pr_hex_upp}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; jtn_types[i].jtn != '\0'; i++)
+		if (jtn[*ind] == jtn_types[i].jtn)
+			return (jtn_types[i].fn(list, buffer, flg, width, prcs, sz));
+
+	if (jtn_types[i].jtn == '\0')
+	{
+		if (jtn[*ind] == '\0')
+			return (-1);
+		unknown_eln += write(1, "%%", 1);
+		if (jtn[*ind - 1] == ' ')
+			unknown_eln += write(1, " ", 1);
+		else if (wdt)
+		{
+			--(*ind);
+			while (jtn[*ind] != ' ' && jtn[*ind] != '%')
+				--(*ind);
+			if (jtn[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknown_eln += write(1, &jtn[*ind], 1);
+		return (unknown_eln);
+	}
+	return (prt_char);
+}
+
